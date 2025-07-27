de742227ff2402168506dad4dde1e8fafba4b6f4
João Siles <joaosiles@gmail.com>
2025-07-26 22:36:15 -0300

    Criação de novos componentes e exemplos sobre a chamada dos mesmos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Home} from './components/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  // template: `
+  //   <h1>Teste</h1>
+  // `
 })
 export class App {
   protected readonly title = signal('estudos-angular');
